d56c40fee17ab95832a25ba65702737efacef729
vagrant <vagrant@localhost.localdomain>
2020-11-04 15:19:22 +0000

    [Add]RSpec_lessons#10

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,17 +1,10 @@
 require 'calc'
 
 RSpec.describe Calc do
+  subject(:calc) { Calc.new } #ローカル変数としてそれぞれのexampleで使用できる・推奨
   it {
-    calc = Calc.new
-    expect(calc.add(2, 3)).to eq(5) # matcher
-    expect(calc.add(2, 3)).not_to eq(5)
-    expect(calc.add(2, 3)).to be true
-    expect(calc.add(2, 3)).to be false
-    expect(calc.add(2, 3)).to be > 10
-    expect(calc.add(2, 3)).to be_between(1, 10).inclusive
-    expect(calc).to respond_to(:add)
-    expect(calc.add(2, 3).integer?).to be true # integer? empty?
-    expect(calc.add(2, 3)).to be_integer # integer?と同じ
+    # calc = Calc.new
+    expect(calc.add(2, 3)).to eq(5)
   }
 end
 
