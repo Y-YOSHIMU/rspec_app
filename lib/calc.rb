df60a23ae559717bb8557fb94d23c67be0855f00
vagrant <vagrant@localhost.localdomain>
2020-11-05 14:42:47 +0000

    [Add]RSpec_lessons#11

--- lib/calc.rb
+++ lib/calc.rb
@@ -2,4 +2,7 @@ class Calc
   def add(a, b)
     a + b #明らかな実装
   end
+  def price(x, tax)
+    x * (1 + tax)
+  end
 end
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -2,10 +2,15 @@ require 'calc'
 
 RSpec.describe Calc do
   subject(:calc) { Calc.new } #ローカル変数としてそれぞれのexampleで使用できる・推奨
-  it {
-    # calc = Calc.new
-    expect(calc.add(2, 3)).to eq(5)
-  }
+  # let
+  context "tax 5%" do
+    let(:tax) { 0.05 }
+    it { expect(calc.price(100, tax)).to eq(105)}
+  end
+  context "tax 8%" do
+    let(:tax) { 0.08 }
+    it { expect(calc.price(100, tax)).to eq(108)}
+  end
 end
 
 # matcher 公式ドキュメント「RSpec Expectations」のモジュール「Built in matchers」
