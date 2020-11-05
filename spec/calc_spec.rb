6c839c2589f0d086656880f41bb0a0d66d32837c
vagrant <vagrant@localhost.localdomain>
2020-11-05 15:28:35 +0000

    [Complete]RSpec_lessons

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,15 +1,20 @@
 require 'calc'
 
-# test double: 代役オブジェクト
-# message expectation: 呼ばれなかったらfail
+RSpec.shared_examples "basic functions" do
+  it "can add"
+  it "can subtrace"
+  it "can multiply"
+  it "can divide"
+end
 
 RSpec.describe Calc do
-  it {
-    logger = double('logger')
-    expect(logger).to receive(:log)
-    calc = Calc.new(logger)
-    expect(calc.add(5, 2)).to eq(7)
-  }
+  context "normal mode" do
+    include_examples "basic functions"
+  end
+  context "expert mode" do
+    include_examples "basic functions"
+    it "can draw graph"
+  end
 end
 
 # matcher 公式ドキュメント「RSpec Expectations」のモジュール「Built in matchers」
