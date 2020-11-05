0c3368a02b9e4dda17df4e95e31927f3efc451e6
vagrant <vagrant@localhost.localdomain>
2020-11-05 15:05:39 +0000

    [Add]RSpec_lessons#13

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,15 +1,17 @@
 require 'calc'
 
+# test double: 代役オブジェクト
+# method stub
+
 RSpec.describe Calc do
-  subject(:calc) { Calc.new } #ローカル変数としてそれぞれのexampleで使用できる・推奨
-  # let
-  # 1. exampleごとに結果がキャッシュされる
-  # 2. 遅延評価
-  let!(:tax) {
-    calc.tax = 0.05
+  it {
+    # userのnameメソッド実装前にテストしたい
+    user = double('user')
+    allow(user).to receive(:name).and_return('yuya')
+    # user.name => yuya 代役オブジェクトができる
+    calc = Calc.new
+    expect(calc.add(5, 2, user.name)).to eq('7 by yuya')
   }
-  it { expect(tax).to eq(0.05) }
-  it { expect(calc.price(100)).to eq(105) }
 end
 
 # matcher 公式ドキュメント「RSpec Expectations」のモジュール「Built in matchers」
